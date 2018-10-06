efd7bef9b97a9e3f52117e90c525c3192623d642
Carlos Eduardo Marciano <carloseduardov8@hotmail.com>
2018-10-06 12:57:06 -0300

    Separate project into classes

--- fetch.py
+++ fetch.py
@@ -5,51 +5,64 @@ import twitter
 from pprint import pprint
 
 
-CONSUMER_KEY = "qqii6KcPy22XPZhFkWZQ5Y77j"
-CONSUMER_SECRET = "stK29zKQaObUoWk3qvYu1Fui4CWhoQkMNyKlnfogsymFX4UmXu"
-OAUTH_TOKEN = "87738700-EAQ3msiJL9ftEnxrezsAT9XhgNpL7IMa0PzE1ze9n"
-OAUTH_TOKEN_SECRET = "BAD3mc4yW1fdofNZ5OClRDjWLOZHHd41CT1H7aqfBgAZ2"
+class Fetch():
+
+	def __init__(self):
+
+		self.twitter_api = None
+		self.tweet_collection = None
+
+		# Sets up key information:
+		CONSUMER_KEY = "qqii6KcPy22XPZhFkWZQ5Y77j"
+		CONSUMER_SECRET = "stK29zKQaObUoWk3qvYu1Fui4CWhoQkMNyKlnfogsymFX4UmXu"
+		OAUTH_TOKEN = "87738700-EAQ3msiJL9ftEnxrezsAT9XhgNpL7IMa0PzE1ze9n"
+		OAUTH_TOKEN_SECRET = "BAD3mc4yW1fdofNZ5OClRDjWLOZHHd41CT1H7aqfBgAZ2"
+
+		# Creates an oauth object to authenticate:
+		auth=twitter.oauth.OAuth(OAUTH_TOKEN,OAUTH_TOKEN_SECRET,CONSUMER_KEY,CONSUMER_SECRET)
+		self.twitter_api=twitter.Twitter(auth=auth)
+
+		# Connects with the database:
+		client=MongoClient()
+		db=client.tweet_db
+		self.tweet_collection = db.tweet_collection
+		self.tweet_collection.create_index([("id",pymongo.ASCENDING)],unique=True)
 
 
-auth=twitter.oauth.OAuth(OAUTH_TOKEN,OAUTH_TOKEN_SECRET,CONSUMER_KEY,CONSUMER_SECRET)
-twitter_api=twitter.Twitter(auth=auth)
+	# Function to use the Twitter API to fetch *count* tweets containing *q*:
+	def fetchTweets(self, q, count=15, save=True):
+
+		search_results = self.twitter_api.search.tweets(count=count,q=q)
+		tweetList = search_results["statuses"]
+		#since_id_new=tweetList[-1]['id']
+
+		if (save == True):
+			for tweet in tweetList:
+				try:
+					self.tweet_collection.insert(tweet)
+				except:
+					pass
 
 
+	# Function to print entries in database:
+	def printTweets(self):
 
-client=MongoClient()
-db=client.tweet_db
-tweet_collection=db.tweet_collection
-tweet_collection.create_index([("id",pymongo.ASCENDING)],unique=True)
+		# Retrievs all data:
+		tweet_cursor = self.tweet_collection.find()
+		# Prints total entries:
+		print(tweet_cursor.count())
+		# Retrieves number of distinct users:
+		user_cursor = self.tweet_collection.distinct("user.id")
+		# Prints number of distinct users:
+		print(len(user_cursor))
 
-
-count=50
-q="%23prayforsyria"
-search_results=twitter_api.search.tweets(count=count,q=q)
-#pprint(search_results['search_metadata'])
-
-
-statuses=search_results["statuses"]
-
-since_id_new=statuses[-1]['id']
-
-for statues in statuses:
-       try:
-              tweet_collection.insert(statues)
-       except:
-              pass
-       
-       
-tweet_cursor=tweet_collection.find()
-print(tweet_cursor.count())
-user_cursor=tweet_collection.distinct("user.id")
-print(len(user_cursor))
-
-for document in tweet_cursor:
-       try:
-              print('-----')
-              print('name:-',document["user"]["name"])
-              print('text:-',document["text"])
-              print('Created Date:-',document["created_at"])
-       except:
-              print("Error in Encoding")
-              pass
+		# Prints all tweets:
+		for document in tweet_cursor:
+			try:
+				print('-----')
+				print('name:-',document["user"]["name"])
+				print('text:-',document["text"])
+				print('Created Date:-',document["created_at"])
+			except:
+				print("Error in Encoding")
+				pass
---
+++ main.py
@@ -0,0 +1,5 @@
+from fetch import Fetch
+
+api = Fetch()
+api.fetchTweets("%23dilma")
+api.printTweets()
