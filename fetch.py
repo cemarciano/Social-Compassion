fa379fcc6c64effe8047f1fc854b1f8713a241d5
Carlos Eduardo Marciano <carloseduardov8@hotmail.com>
2018-10-17 19:34:07 -0300

    Migrate project to Twitter Scraper

--- fetch.py
+++ fetch.py
@@ -1,73 +1,79 @@
-import pymongo
-from pymongo import MongoClient
-import json
-import twitter
-from bson.son import SON
+from twitterscraper import query_tweets
+from bs4 import BeautifulSoup
 from pprint import pprint
+import datetime as dt
+import pymongo
+import sys
+import codecs
+import json
+
 
 
 class Fetch():
 
+	# Constructor to initialize attributes:
 	def __init__(self):
 
-		self.twitter_api = None
-		self.tweet_collection = None
-
-		# Sets up key information:
-		CONSUMER_KEY = "qqii6KcPy22XPZhFkWZQ5Y77j"
-		CONSUMER_SECRET = "stK29zKQaObUoWk3qvYu1Fui4CWhoQkMNyKlnfogsymFX4UmXu"
-		OAUTH_TOKEN = "87738700-EAQ3msiJL9ftEnxrezsAT9XhgNpL7IMa0PzE1ze9n"
-		OAUTH_TOKEN_SECRET = "BAD3mc4yW1fdofNZ5OClRDjWLOZHHd41CT1H7aqfBgAZ2"
-
-		# Creates an oauth object to authenticate:
-		auth=twitter.oauth.OAuth(OAUTH_TOKEN,OAUTH_TOKEN_SECRET,CONSUMER_KEY,CONSUMER_SECRET)
-		self.twitter_api=twitter.Twitter(auth=auth)
+		# Database handler:
+		self.db = None
 
 		# Connects with the database:
-		client=MongoClient()
-		db=client.tweet_db
-		self.tweet_collection = db.tweet_collection
-		self.tweet_collection.create_index([("id",pymongo.ASCENDING)],unique=True)
+		client = pymongo.MongoClient()
+		db = client.tweet_db
+		self.collection = db.tweet_collection
+		self.collection.create_index([("id",pymongo.ASCENDING)],unique=True)
+
+		# Corrects encoding to UTF-8:
+		if sys.stdout.encoding != 'UTF-8':
+			sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer, 'strict')
 
 
-	# Function to use the Twitter API to fetch *count* tweets containing *q*:
-	def fetchTweets(self, q, count=15, save=True):
+	# Fetches at least *count* tweets containing the words *q*:
+	def fetchTweets(self, q, count, begindate=dt.date(2006,3,21), enddate=dt.date.today(), save=True, printTweets=False):
 
-		search_results = self.twitter_api.search.tweets(count=count,q=q)
-		tweetList = search_results["statuses"]
-		#since_id_new=tweetList[-1]['id']
+		# Function to return the language attribute out of an HTML containing a <p> element:
+		def getLang(string):
+			soup = BeautifulSoup(string, 'lxml')
+			return soup.find('p')["lang"]
 
+		# Performs the query:
+		list_of_tweets = query_tweets(q, count, begindate=begindate, enddate=enddate)
+
+		# Checks if saving to database is enabled:
 		if (save == True):
-			for tweet in tweetList:
+			# Loops through obtained tweets:
+			for tweet in list_of_tweets:
+
+				# Treats newline and hashtags:
+				tweet.text = tweet.text.replace('\n', ' ').replace('\r', ' ')
+
+				# Checks if printing is enabled:
+				if (printTweets == True):
+					print( '@%s tweeted: %s \n' % ( tweet.user, tweet.text ) )
+
+				# Converts Tweet object to JSON:
+				tweetDict = {
+						"id" : tweet.id,
+						"text" : tweet.text,
+						"user" : tweet.user,
+						"fullname" : tweet.fullname,
+						"lang" : getLang(tweet.html),
+						"replies" : tweet.replies,
+						"retweets" : tweet.retweets,
+						"likes" : tweet.likes,
+						"timestamp" : tweet.timestamp
+						}
+				tweetJson = json.dumps(tweetDict, default=str)
+
+				# Attempts to save in database:
 				try:
-					self.tweet_collection.insert(tweet)
+					self.collection.insert(tweetDict)
 				except:
+					print("Database insertion failed.")
 					pass
 
 
-	# Function to print entries in database:
-	def printTweets(self):
-
-		# Retrievs all data:
-		tweet_cursor = self.tweet_collection.find()
-		# Prints total entries:
-		print(tweet_cursor.count())
-		# Retrieves number of distinct users:
-		user_cursor = self.tweet_collection.distinct("user.id")
-		# Prints number of distinct users:
-		print(len(user_cursor))
-
-		# Prints all tweets:
-		for document in tweet_cursor:
-			try:
-				print('-----')
-				print('name:-',document["user"]["name"])
-				print('text:-',document["text"])
-				print('Created Date:-',document["created_at"])
-			except:
-				print("Error in Encoding")
-				pass
-
+	# Function to print the first *count* most used words in the db collection:
 	def printMostUsed(self, count=5):
 
 		# Prepare query:
@@ -78,7 +84,7 @@ class Fetch():
 		]
 
 		# Retrieve count of words:
-		results = list(self.tweet_collection.aggregate(query))
+		results = list(self.collection.aggregate(query))
 
 		# Sort result (so most used words are displayed first):
 		results.sort(key=lambda x: x["count"], reverse=True)
@@ -86,7 +92,7 @@ class Fetch():
 		# Print requested results:
 		for i in range(count):
 			try:
-				pprint(results[i])
+				print ( 'Word \"%s\" appears %s times' % ( results[i]["_id"], results[i]["count"] ) )
 			except:
-				print("Error in encoding")
+				print("Error in encoding when printing most used words")
 				pass
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import datetime as dt
 from fetch import Fetch
 
-api = Fetch()
-api.printMostUsed(12)
-#api.fetchTweets("%23dilma")
-#api.printTweets()
+
+
+if __name__ == '__main__':
+
+	# Performs queries:
+	fetchy = Fetch()
+	#fetchy.fetchTweets("prayforsyria", 300000, begindate=dt.date(2011,1,21), enddate=dt.date.today())
+	fetchy.printMostUsed(200)
+	#Or save the retrieved tweets to file:
+	#file = open("output.txt","w", encoding="utf-8")
